f3b4842582fec08f3bda9fbb1b927054abd6debd
RCRalph <rcralph@rcralph.me>
2026-04-29 13:25:07 +0200

    feat: Added order randomization and timer shared between scenes

--- LevelUI/order_variant.gd
+++ LevelUI/order_variant.gd
@@ -1,23 +1,29 @@
-class_name Order
+class_name OrderVariant
 extends RefCounted
 
 const OrderIngredient = preload("res://LevelUI/Types/order_ingredient.gd")
 
-var id: String
 var texture_path: String
-var start_time: float
-var duration: float
 var ingredients: Array[OrderIngredient]
 
+
 func _init(
-	_id: String,
 	_texture_path: String,
-	_start_time: float,
-	_duration: float,
 	_ingredients: Array[OrderIngredient]
 ) -> void:
-	id = _id
 	texture_path = _texture_path
-	start_time = _start_time
-	duration = _duration
 	ingredients = _ingredients
+
+
+func create_order(
+	id: String,
+	start_time: float,
+	duration: float
+) -> Order:
+	return Order.new(
+		id,
+		texture_path,
+		start_time,
+		duration,
+		ingredients
+	)
