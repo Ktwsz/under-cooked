8a0199a0d4abba3f745c2426b351973242d7e580
Ktwsz <doroszuk20@gmail.com>
2026-05-09 11:49:59 +0200

    add level 2

--- PlateTable.cs
+++ PlateTable.cs
@@ -6,13 +6,11 @@ public partial class PlateTable : Table
 {
 	private Stack _plates = new Stack();
 	private float _stackHeight = 0;
-	private Timer _timer;
 	private PackedScene _plateScene = ResourceLoader.Load<PackedScene>("res://plate.tscn");
 
 	public override void _Ready()
 	{
-		_timer = GetNode<Timer>("Timer");
-		_timer.Timeout += OnTimerTimeout;
+		GetNode<Timer>("Timer").Timeout += OnTimerTimeout;
 	}
 
 	public override Node3D PickupItem()
@@ -33,14 +31,14 @@ public partial class PlateTable : Table
 		var newPlate = _plateScene.Instantiate() as Plate;
 		AddChild(newPlate);
 		newPlate.SetPosition(new Vector3(0, _stackHeight, 0));
-		_stackHeight += ExtractSize(newPlate) + 0.1f;
+		_stackHeight += ExtractSize(newPlate) + 0.02f;
 		_plates.Push(newPlate);
 	}
 
 	private Plate PopPlate()
 	{
 		var lastPlate = _plates.Pop() as Plate;
-		_stackHeight -= ExtractSize(lastPlate) + 0.1f;
+		_stackHeight -= ExtractSize(lastPlate) + 0.02f;
 		return lastPlate;
 	}
 
